a775ea033140a1fd63316865810b94afc66a64b9
Onepo1ntFive <name.diesel@gmail.com>
2022-01-16 13:24:34 +0300

    Вёрстка

--- src/js/part/gallery.js
+++ src/js/part/gallery.js
@@ -1,31 +1,30 @@
 (function () {
+    if (!!document.querySelector('.swiper-gallery')) {
+        let windowWidth = window.innerWidth;
+        let swiperGallery = undefined;
 
-    let windowWidth = window.innerWidth;
-    let swiper = undefined;
+        function initGallery() {
+            windowWidth = window.innerWidth;
 
-    function initGallery() {
-        windowWidth = window.innerWidth;
-
-        if (windowWidth >= 768 && swiper !== undefined) {
-            console.log('destroy')
-            swiper.destroy(false, true);
-            swiper = undefined;
-        } else if (windowWidth < 768 && swiper === undefined) {
-            console.log('init')
-            swiper = new Swiper('.swiper', {
-                spaceBetween: 100,
-                pagination: {
-                    el: '.swiper-pagination',
-                    type: 'bullets',
-                    clickable: true,
-                },
-            });
+            if (windowWidth >= 768 && swiperGallery !== undefined) {
+                swiperGallery.destroy(false, true);
+                swiperGallery = undefined;
+            } else if (windowWidth < 768 && swiperGallery === undefined) {
+                swiperGallery = new Swiper('.swiper-gallery', {
+                    spaceBetween: 100,
+                    pagination: {
+                        el: '.swiper-pagination',
+                        type: 'bullets',
+                        clickable: true,
+                    },
+                });
+            }
         }
-    }
 
-    initGallery();
-    window.addEventListener('resize', function (event) {
-        initGallery()
-    }, true);
+        initGallery();
+        window.addEventListener('resize', function () {
+            initGallery()
+        }, true);
+    }
 
 })();
---
+++ src/js/part/reviews.js
@@ -0,0 +1,15 @@
+(function () {
+    if (!!document.querySelector('.swiper-reviews')) {
+        const swiperReviews = new Swiper('.swiper-reviews', {
+            spaceBetween: 20,
+            loop: true,
+            speed: 800,
+            autoHeight: true,
+            watchSlidesProgress: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
+})();
